e29785e76ed14143f46816e10b9905b8e11d89ad
requiem-of-zero <swonggzz@gmail.com>
2022-12-24 09:11:17 -0800

    Created project skeleton, header complete

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    domains: ["rb.gy"],
+    formats: ["image/avif", "image/webp"],
+    dangerouslyAllowSVG: true,
+  },
   reactStrictMode: true,
-}
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
+import Image from 'next/image'
+import Link from 'next/link'
+const Header = () => {
+  return (
+    <header>
+      <div className='flex items-center space-x-2 md:space-x-10'>
+        <Image
+            src="https://rb.gy/ulxxee"
+            width={100}
+            height={100}
+            className="cursor-pointer object-contain"
+            alt='kreed homepage logo'
+          />
+      <ul className='hidden space-x-4 md:flex'>
+        <li className='header__link'>Home</li>
+        <li className='header__link'>TV Shows</li>
+        <li className='header__link'>Movies</li>
+        <li className='header__link'>New & Popular</li>
+        <li className='header__link'>My List</li>
+      </ul>
+      </div>
+      <div className='flex items-center space-x-4 text-sm font-light'>
+        <MagnifyingGlassIcon className='hidden h-6 w-6 sm:inline'/>
+        <p className='hidden lg:inline'>Kids</p>
+        <BellIcon className='h-6 w-6 sm:inline'/>
+
+        <Link href="/account">
+          <Image 
+            src='https://rb.gy/g1pwyx'
+            width='30'
+            height='30'
+            className='cursor-pointer rounded'
+            alt='demo profile image'
+          />
+        </Link>
+      </div>
+    </header>
+  )
+}
+
+export default Header
