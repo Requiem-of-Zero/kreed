66d3864cc873e37104d68d8a6b5d94bbed4c1405
requiem-of-zero <swonggzz@gmail.com>
2022-12-25 11:19:36 -0800

    Completed homepage banner UI

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,7 @@
+import { InformationCircleIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import { FaPlay } from "react-icons/fa";
 import { IMAGE_BASE_URL } from "../../constants/media";
 import { Movie } from "../../typings";
 
@@ -17,26 +19,35 @@ const Banner = ({ netflixOriginals }: Props) => {
   }, [netflixOriginals]);
 
   return (
-    <div className="relative">
-      <div className="absolute top-0 left-0 h-[95vh] w-screen">
+    <div className="relative flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+      <div className="z-0 absolute top-0 left-0 h-[95vh] w-screen select-none">
         <Image
           src={`${IMAGE_BASE_URL}/${
             featuredMovie?.backdrop_path || featuredMovie?.poster_path
           }`}
           object-fit="contain"
           fill
-          alt=""
+          alt="featured backdrop"
         />
       </div>
-      <div className="absolute">
-        <h1 className="font-bold text-2xl md:text-4xl lg:text-7xl">
+      <div className="bannerCTA z-10 pl-10 lg:w-200">
+        <h1 className="font-bold text-2xl pb-4 md:text-4xl lg:text-7xl">
           {featuredMovie?.title ||
             featuredMovie?.name ||
             featuredMovie?.original_title}
         </h1>
-        <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        <p className="max-w-xs pb-5 text-shadow-md text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
           {featuredMovie?.overview}
         </p>
+        <div className="flex space-x-3">
+          <button className="bannerBtn bg-white text-black">
+            <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play
+          </button>
+          <button className="bannerBtn bg-[gray]/70">
+            More Into{" "}
+            <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" />
+          </button>
+        </div>
       </div>
     </div>
   );
