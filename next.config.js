867aed0e8af1161b7518a60082ae5bb79d465a7b
requiem-of-zero <swonggzz@gmail.com>
2022-12-25 15:22:07 -0800

    New config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ module.exports = {
   images: {
     domains: [
       "image.tmdb.org",
-      "https://lh3.googleusercontent.com/",
     ],
     formats: ["image/avif", "image/webp"],
     dangerouslyAllowSVG: true,
