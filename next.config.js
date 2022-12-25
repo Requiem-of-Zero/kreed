a1ced9569c70316348f9fa7f63c375f62c3e5fb1
requiem-of-zero <swonggzz@gmail.com>
2022-12-25 14:39:37 -0800

    Fix for deployed site

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ["rb.gy", "image.tmdb.org", 'imgur.com'],
+    domains: ["rb.gy", "image.tmdb.org", 'ibb.co'],
     formats: ["image/avif", "image/webp"],
     dangerouslyAllowSVG: true,
   },
