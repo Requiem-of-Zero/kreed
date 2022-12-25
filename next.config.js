dc1c481e304bd659eea3db74f56caf0e346faaf9
requiem-of-zero <swonggzz@gmail.com>
2022-12-25 14:32:55 -0800

    Updated authorized domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ["rb.gy", "image.tmdb.org"],
+    domains: ["rb.gy", "image.tmdb.org", 'imgur.com'],
     formats: ["image/avif", "image/webp"],
     dangerouslyAllowSVG: true,
   },
