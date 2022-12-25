cf1a8da915ba528ada5ef57d056deb8cbac3092f
requiem-of-zero <swonggzz@gmail.com>
2022-12-25 15:15:25 -0800

    Added purge lines to fix deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/Components/**/*.{js,ts,jsx,tsx}"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
