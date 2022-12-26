54dfc55991c3a155ef94ea10fcd6b74fffc75fa8
requiem-of-zero <swonggzz@gmail.com>
2022-12-25 16:01:38 -0800

    Removed unused authorized domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ["image.tmdb.org", "drive.google.com"],
+    domains: ["image.tmdb.org"],
     formats: ["image/avif", "image/webp"],
     dangerouslyAllowSVG: true,
   },
