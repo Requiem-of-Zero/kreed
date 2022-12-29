050afe2a3017d303bf8ae83f5fb97be9d7605ccc
requiem-of-zero <swonggzz@gmail.com>
2022-12-29 14:04:30 -0800

    Updated path bugs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  basePath: '/home',
   images: {
     domains: ["image.tmdb.org", 'api.themoviedb.org'],
     formats: ["image/avif", "image/webp"],
