52d400473f917c90a5cbc59c482935e3bcb9c509
requiem-of-zero <swonggzz@gmail.com>
2022-12-29 15:23:54 -0800

    Fixed open modal scroll interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,7 +72,7 @@ const Home = ({
 
   return (
     <div
-      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[100vh] ${
         showModal && "h-screen overflow-hidden"
       }`}
     >
@@ -81,7 +81,7 @@ const Home = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main>
+      <main className='pb-2'>
         <Banner netflixOriginals={netflixOriginals} />
         <section className="absolute md:space-y-24 md:pt-40">
           <MediaRows title="Trending Now" movies={trendingNow} />
