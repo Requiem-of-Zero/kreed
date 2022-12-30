5f3696bcb130b7fba55eea98fcaf6a7aaf49b9cb
requiem-of-zero <swonggzz@gmail.com>
2022-12-29 23:54:46 -0800

    changed to ts file to make routes happy

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -136,7 +136,7 @@ const Modal = () => {
             }`}
             width="100%"
             height="100%"
-            style={{ position: "absolute", top: "0", left: "0" }}
+            style={{ position: "absolute", top: "0", left: "0", backgroundColor: 'black' }}
             playing
             muted={muted}
           />
