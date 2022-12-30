42d83490b6d943255b89505053b03306b7925bf3
requiem-of-zero <swonggzz@gmail.com>
2022-12-29 23:45:09 -0800

    Added conversion before query test

--- pages/api/movie/[movieId].tsx
+++ pages/api/movie/[movieId].tsx
@@ -3,11 +3,11 @@ import { prisma } from "../../../lib/prisma";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { movieId } = req.query;
-  
+  const numId = Number(movieId)
   try {
     const data = await prisma.comment.findMany({
       where: {
-        movieId: Number(movieId),
+        movieId: numId,
       },
     });
     res.status(200).json(data);
